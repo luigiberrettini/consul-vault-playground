acdec14c4c1d469463ffeef59a5e5eabaacea883
Luigi Berrettini <luigi.berrettini@gmail.com>
2016-07-19 16:36:30 +0200

    Add container for HTTP API

--- vault-http-api.sh
+++ vault-http-api.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+function startVaultServerForApi()
+{
+    printf "***** Starting a Vault Server in standard mode using file storage backend\n"
+
+    local configFolder=$1
+    local containerName='vaultapi'
+
+    docker run -v $configFolder:/config -p 8241:8200 --name $containerName -h $containerName -d sjourdan/vault server -config=/config/file.hcl
+    sleep 2
+    _printLogs $containerName
+}
+
 function checkInitStatus()
 {
     printf "***** [HTTP-API] Checking Vault server init status\n"
 
-    # 8221
+    # 8241
     local port=$1
     curl --silent "http://127.0.0.1:$port/v1/sys/init" | jq
 }
